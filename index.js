ce4fcd75562657b099c120be1a3dc8a6145fc702
SourabhBurman <souravburman55@gmail.com>
2024-02-14 16:29:10 +0530

    latest

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require("express")
 const app= express()
 const {connection} = require("./db")
 const userRouter = require("./Routes/user.Route")
-const auth = require("./middleware/auth.middleware")
 const { noteRouter } = require("./Routes/note.Route")
+require('dotenv').config();
 app.use(express.json())
 app.use("/users",userRouter)   
 app.use('/notes',noteRouter)
 
 
-app.listen(8080,async()=>{
+app.listen(process.env.PORT,async()=>{
     try{
         await connection
-        console.log("server running at port : http://localhost:8080")
+        console.log(`server running at port : http://localhost:${process.env.PORT}`)
     }catch(err){
         console.log(err)}   
 })
